4e4793f34554d2266811f5d585da53036604523e
Xan Johnson <xan@smarty.com>
2024-11-05 10:24:13 -0700

    Add other operators

--- calc_test.go
+++ calc_test.go
@@ -23,3 +23,71 @@ func TestAddition_Calculate(t *testing.T) {
 		})
 	}
 }
+
+func TestSubtraction_Calculate(t *testing.T) {
+	tests := []struct {
+		a, b, want int
+	}{
+		{a: 0, b: 0, want: 0},
+		{a: 0, b: 1, want: -1},
+		{a: 4, b: 5, want: -1},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d - %d = %d", tt.a, tt.b, tt.want), func(t *testing.T) {
+			subtraction := &Subtraction{}
+			got := subtraction.Calculate(tt.a, tt.b)
+			if got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMultiplication_Calculate(t *testing.T) {
+	tests := []struct {
+		a, b, want int
+	}{
+		{a: 0, b: 0, want: 0},
+		{a: 0, b: 1, want: 0},
+		{a: 4, b: 5, want: 20},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d * %d = %d", tt.a, tt.b, tt.want), func(t *testing.T) {
+			multiplication := &Multiplication{}
+			got := multiplication.Calculate(tt.a, tt.b)
+			if got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDivision_Calculate(t *testing.T) {
+	tests := []struct {
+		a, b, want int
+	}{
+		{a: 0, b: 0, want: 0},
+		{a: 6, b: 3, want: 2},
+		{a: 30, b: 6, want: 5},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d / %d = %d", tt.a, tt.b, tt.want), func(t *testing.T) {
+			division := &Division{}
+			got := division.Calculate(tt.a, tt.b)
+			if got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDivision_ByZeroPanics(t *testing.T) {
+	division := &Division{}
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Fatalf("Division.ByZero did not panic")
+		}
+	}()
+	division.Calculate(1, 0)
+}
